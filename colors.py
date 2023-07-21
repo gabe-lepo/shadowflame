15e5fd249a89c346f7931d9047733ec1d43cdd23
gabe-lepo <gabelepoutre@gmail.com>
2023-07-21 15:31:23 -0400

    removed colors.py

--- colors.py
+++
@@ -1,7 +0,0 @@
-class Colors():
-    def __init__(self):
-        self.RED = (255, 0, 0)
-        self.GREEN = (0, 255, 0)
-        self.BLUE = (0, 0, 255)
-        self.WHITE = (255, 255, 255)
-        self.BLACK = (0, 0, 0)
