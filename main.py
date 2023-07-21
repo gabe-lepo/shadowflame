9920c5e313b8dd8a0e9c68fb4448d88c756fd44a
gabe-gardyn <gabriel@gardyn.tech>
2023-07-21 14:50:58 -0400

    added screen class

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from pygame.locals import *
 from globals import *
 from player import Player
 from colors import Colors
+from screen import Screen
 
 #Initialization
 pg.init()
-display_info = pg.display.Info()
-current_width, current_height = display_info.current_w, display_info.current_h
-#screen = pg.display.set_mode((current_width, current_height), pg.FULLSCREEN)
-screen = pg.display.set_mode((WIDTH, HEIGHT))
+main_screen = Screen(fullscreen=False)
 
 # Setup objects
 p1 = Player()
@@ -25,15 +23,15 @@ while running:
          if event.key == K_ESCAPE:
             running = False
    
-   screen.fill(colors.BLACK)
+   main_screen.display.fill(colors.BLACK)
    #vert grid
    #for _ in range(1, GRID_DEPTH, 1):
-   #   pg.draw.line(screen, (255, 255, 255), (current_width * (_/GRID_DEPTH), 0), (current_width * (_/GRID_DEPTH), current_height))
+   #   pg.draw.line(main_screen.display, (255, 255, 255), (current_width * (_/GRID_DEPTH), 0), (current_width * (_/GRID_DEPTH), current_height))
    #horiz grid
    #for _ in range(1, GRID_DEPTH, 1):
-   #   pg.draw.line(screen, (255, 255, 255), (0, current_height * (_/GRID_DEPTH)), (current_width, current_height * (_/GRID_DEPTH)))
+   #   pg.draw.line(main_screen.display, (255, 255, 255), (0, current_height * (_/GRID_DEPTH)), (current_width, current_height * (_/GRID_DEPTH)))
 
-   screen.blit(p1.surface, p1.center_coord)
+   main_screen.display.blit(p1.surface, p1.center_coord)
 
    pg.display.update()
 
