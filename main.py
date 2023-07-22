242ca02103e70d287b548fa4b47d4a8072a17bc9
gabe-lepo <gabelepoutre@gmail.com>
2023-07-21 20:01:16 -0400

    testing stuff

---
+++ main.py
@@ -0,0 +1,9 @@
+from player import Player
+
+p1 = Player()
+p2 = Player()
+p3 = Player()
+p4 = Player()
+
+p2.health -= 50
+print("p1 health:{} | p2 health:{}".format(p1.health, p2.health))
--- player.py
+++ player.py
@@ -2,5 +2,10 @@ import pygame as pg
 from pygame.locals import *
 
 class Player():
-    def __init__(self) -> None:
-        pass
+   def __init__(self) -> None:
+      self.health = 100
+   
+   def check_dead(self):
+      if self.health == 0:
+         #do stuff
+         self.health = 100
